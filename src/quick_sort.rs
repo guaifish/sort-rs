f266db3521b28896d0b2a20a4408440c87f310fd
guaifish <guaifish@hotmail.com>
2020-09-22 16:21:19 +0800

    update quick sort

--- src/quick_sort.rs
+++ src/quick_sort.rs
@@ -6,20 +6,20 @@ impl<T: Ord + Copy> QuickSort<T> for [T] {
     fn quick_sort(&mut self) {
         let len = self.len();
         if len > 1 {
-            let mut left = 0;
-            let mut right = len - 1;
-            while left < right {
-                while left < right && self[right] >= self[0] {
-                    right -= 1;
+            let mut l = 0;
+            let mut r = len - 1;
+            while l < r {
+                while l < r && self[r] >= self[0] {
+                    r -= 1;
                 }
-                while left < right && self[left] <= self[0] {
-                    left += 1;
+                while l < r && self[l] <= self[0] {
+                    l += 1;
                 }
-                self.swap(left, right);
+                self.swap(l, r);
             }
-            self.swap(0, left);
-            self[..left].quick_sort();
-            self[(left + 1)..].quick_sort();
+            self.swap(0, l);
+            self[..l].quick_sort();
+            self[(l + 1)..].quick_sort();
         }
     }
 }

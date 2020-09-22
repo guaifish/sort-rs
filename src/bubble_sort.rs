649daa79fc9c3a10664c276354c8cb007791de62
guaifish <guaifish@hotmail.com>
2020-09-22 20:02:55 +0800

    update bubble sort

--- src/bubble_sort.rs
+++ src/bubble_sort.rs
@@ -5,8 +5,9 @@ pub trait BubbleSort<T: Ord + Copy> {
 impl<T: Ord + Copy> BubbleSort<T> for [T] {
     #[inline]
     fn bubble_sort(&mut self) {
-        for i in 0..self.len() {
-            for j in 1..(self.len() - i) {
+        let len = self.len();
+        for i in 0..len {
+            for j in 1..(len - i) {
                 if self[j - 1] > self[j] {
                     self.swap(j - 1, j);
                 }

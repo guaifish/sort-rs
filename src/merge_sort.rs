9636f95923a15a6a2cd84489a4f038d20921a0af
guaifish <guaifish@hotmail.com>
2020-09-22 11:46:15 +0800

    update merge sort

--- src/merge_sort.rs
+++ src/merge_sort.rs
@@ -9,23 +9,14 @@ impl<T: Ord + Copy> MergeSort<T> for [T] {
             let mid = len / 2;
             self[..mid].merge_sort();
             self[mid..].merge_sort();
-            let mut head = 0;
             let mut l = 0;
             let mut r = mid;
-            while l < mid && r < len {
-                if self[head] > self[r] {
-                    let tmp = self[r];
-                    let mut i = r;
-                    while i > head {
-                        self[i] = self[i - 1];
-                        i -= 1;
-                    }
-                    self[head] = tmp;
+            while l < r && r < len {
+                if self[r] < self[l] {
+                    self[l..=r].rotate_right(1);
                     r += 1;
-                } else {
-                    l += 1;
                 }
-                head += 1;
+                l += 1;
             }
         }
     }

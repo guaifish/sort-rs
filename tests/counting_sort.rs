0d88b958a53a685c84313887a741639caab92fc4
guaifish <guaifish@hotmail.com>
2020-09-24 20:13:52 +0800

    fix insertion sort

--- tests/counting_sort.rs
+++ tests/counting_sort.rs
@@ -4,24 +4,24 @@ use sorted::CountingSort;
 #[test]
 fn test_counting_sort_vec() {
     let mut v = vec![6, 5, 3, 1, 8, 7, 2, 4, -7, -10, 0, 6, 1];
-    v.counting_sort(-10, 8);
+    v.counting_sort();
     let expected = vec![-10, -7, 0, 1, 1, 2, 3, 4, 5, 6, 6, 7, 8];
     assert_eq!(v, expected);
 
     let mut v: Vec<usize> = vec![6, 5, 3, 1, 8, 7, 2, 4];
-    v.counting_sort(1, 8);
+    v.counting_sort();
     let expected = vec![1, 2, 3, 4, 5, 6, 7, 8];
     assert_eq!(v, expected);
 }
 #[test]
 fn test_counting_sort_array() {
     let mut v = [6, 5, 3, 1, 8, 7, 2, 4, -7, -10, 0, 6, 1];
-    v.counting_sort(-10, 8);
+    v.counting_sort();
     let expected = [-10, -7, 0, 1, 1, 2, 3, 4, 5, 6, 6, 7, 8];
     assert_eq!(v, expected);
 
     let mut v: [usize; 8] = [6, 5, 3, 1, 8, 7, 2, 4];
-    v.counting_sort(1, 8);
+    v.counting_sort();
     let expected = [1, 2, 3, 4, 5, 6, 7, 8];
     assert_eq!(v, expected);
 }
@@ -30,7 +30,7 @@ fn test_counting_sort_array() {
 fn test_counting_sort_slice() {
     let mut a = [6, 5, 3, 1, 8, 7, 2, 4];
     let s: &mut [i32] = &mut a[..];
-    s.counting_sort(1, 8);
+    s.counting_sort();
     let expected = [1, 2, 3, 4, 5, 6, 7, 8];
     assert_eq!(*s, expected);
 }
@@ -41,6 +41,6 @@ fn test_counting_sort_rand() {
     let mut nums: Vec<i32> = (-100..=100).flat_map(|x| [x].repeat(3)).collect();
     let expected = nums.clone();
     nums.shuffle(&mut rng);
-    nums.counting_sort(-100, 100);
+    nums.counting_sort();
     assert_eq!(nums, expected);
 }
